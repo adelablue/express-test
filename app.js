b8baeeddf93dcd5a45023d311e8ddd3a581d0a6f
阿呆拉 <adelachen@MacBook-Pro-2.local>
2019-06-29 14:24:38 +0800

    add ejs view

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ let app = express();
 
 const port = 8000;
 
+app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => res.send('<b>GREAT</b>'));
+//app.get('/', (req, res) => res.send('<b>GREAT</b>'));
+app.get('/', (req, res) => {
+    res.render('index', {message: 'hello ejs'});
+});
 
 app.listen(port, ()=> {console.log(`listening at the ${port}`)});
