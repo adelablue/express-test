7f2f4340c5ab6c2f49e1d63063b6bb9dfe4fa783
阿呆拉 <adelachen@MacBook-Pro-2.local>
2019-06-30 00:31:28 +0800

    dynamic routing

--- src/routes/token-routes.js
+++ src/routes/token-routes.js
@@ -1,7 +1,31 @@
 let tokens = require('../data/token-repository');
 
+let sendDetails = (id, res) => {
+
+    let token = tokens.getById(id);
+
+    if (!token) {
+        res.sendStatus(404);
+    } else {
+        res.render('tokens/details', {token: token});
+    }
+
+};
+
+
 module.exports = (app) => {
     app.get('/tokens', (req, res) => {
-        res.render('tokens/index', {tokens: tokens.all()});
+        let id = req.query.id;
+
+        if (id) {
+            sendDetails(id, res);
+        } else {
+            res.render('tokens/index', {tokens: tokens.all()});
+        }
+
     });
+
+    app.get('/tokens/:id', (req, res) => {
+        sendDetails(req.params.id, res);
+    })
 }
